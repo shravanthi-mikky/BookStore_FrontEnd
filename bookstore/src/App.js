fc6a96499fb8289417f06c03ba59bf96aae49c1a
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-09-13 13:43:05 +0530

    [shravanthi] - align items in center

---
+++ bookstore/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './Pages/Login/Login.jsx'
+
+function App() {
+  return (
+    <div>
+      <Login/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ bookstore/src/Pages/Login/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Login.css'
+
+function Login() {
+  return (
+    <div className='LoginContainer'>
+     <div>1</div>
+     <div>2</div>
+     <div>3</div>
+     <div>4</div>
+    </div>
+  )
+}
+
+export default Login
